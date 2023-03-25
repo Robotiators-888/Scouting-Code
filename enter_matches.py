92288b9b6ae34499fa7beed3fc080c78dcb873b5
Kenneth Wang <109991402+kennlw@users.noreply.github.com>
2023-03-25 15:00:53 -0400

    changed

--- enter_matches.py
+++
@@ -1,7 +0,0 @@
-num_matches = int(input("How many matches?: "))
-match_dict = {}
-for i in range(num_matches):
-    x = input(f"Team nums for match {i+1} (separated by spaces): ")
-    xls = list(x.split(" "))
-    match_dict[i+1] =  xls
-print(match_dict)
