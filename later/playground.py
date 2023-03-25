4f176e4e8bc11e1a2eedecd66308ddc545245049
Kenneth Wang <109991402+kennlw@users.noreply.github.com>
2023-03-25 09:44:42 -0400

    normalize data

--- later/playground.py
+++ later/playground.py
@@ -1,1 +1,5 @@
-#^._.^
+import pandas as pd
+df = pd.DataFrame({'month': [1, 4, 7, 10],
+                   'year': [2012, 2014, 2013, 2014],
+                   'sale': [55, 40, 84, 31]})
+print(list(df))
